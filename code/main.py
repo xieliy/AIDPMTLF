6d04838e3e39aeac23f711de75cab055c8fa9166
xieliy <xieliyan@msu.edu>
2018-12-02 19:10:02 -0500

    Add preprocessing of data

--- code/main.py
+++ code/main.py
@@ -2,6 +2,11 @@ import csv
 from task import task
 from server import server
 from multiprocessing import Queue, Lock, Process
+import numpy as np
+
+# Parameters that need to adjust
+dataset = 'synthetic' # 'real'
+privacy = 0.005 # [0.005, 0.001, 0.05, 0.1]
 
 path_project = '/home/xieliyan/Dropbox/GPU/GPU1/AIDPMTLF/' # path of project
 path_data = path_project + "data/" # path of data
@@ -11,10 +16,16 @@ wait_time = [i+1 for i in xrange(T)] # waiting time in one iteration, each task
 p_train = 0.7 # percentage of training set
 Lambda = 0.001 # regularization parameter
 d = 28
-ITER = 3000
+ITER = 1000
 p_ite = 200
 step_task = 0.1
 step_server = 0.01
+mu = 0.0 # Gaussisan noise
+alpha = 10.0
+C1 = 1.0 # data is normalized to 1.0
+C2 = 0.25 # bound on the derivavtive of logistic loss
+data_num = 200 # number of data points in each local task
+sigma = np.sqrt((2.0 * ITER * alpha * (step_server ** 2) * (C1 ** 2) * (C2 ** 2)) / (privacy * (data_num ** 2)))
 data_all = [[] for i in xrange(T)] # data for all tasks
 label_all = [[] for i in xrange(T)] # label for all tasks
 tasks = [] # create tasks
@@ -22,27 +33,57 @@ tasks = [] # create tasks
 lock = Lock()
 
 conn = Queue() # creating a Queue between tasks and server
-server_ins = server(path_results, conn, lock, d, step_server, Lambda, T) # create server
-for k in xrange(T):
-    with open(path_data + 'data' + str(k + 1), 'rb') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            data_all[k].append(row[0].split(' '))
-    data_all[k].pop(0) # remove first one
+server_ins = server(path_results, conn, lock, d, step_server, Lambda, T, mu, sigma) # create server
 
-    N = len(data_all[k])
-    for i in xrange(N):
-        del data_all[k][i][-1]
-        data_all[k][i] = [float(data_all[k][i][j]) for j in xrange(d)]
+if dataset == 'synthetic':
+    for k in xrange(T):
+        with open(path_data + 'data' + str(k + 1), 'rb') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                data_all[k].append(row[0].split(' '))
+        data_all[k].pop(0) # remove first one
 
-    with open(path_data + 'labelc' + str(k + 1), 'rb') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            label_all[k].append(float(row[0][:-1]))
-    label_all[k].pop(0)
+        N = len(data_all[k])
+        for i in xrange(N):
+            del data_all[k][i][-1]
+            data_all[k][i] = [float(data_all[k][i][j]) for j in xrange(d)]
 
-    tasks.append(task(path_results, data_all[k], label_all[k], conn, k, Lambda, ITER, p_ite, step_task, d, p_train, wait_time[k]))
-    tasks[-1].model_init()
+        with open(path_data + 'labelc' + str(k + 1), 'rb') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                label_all[k].append(float(row[0][:-1]))
+        label_all[k].pop(0)
+
+        tasks.append(task(path_results, dataset, data_all[k], label_all[k], conn, k, Lambda, ITER, p_ite, step_task, d, p_train, wait_time[k]))
+        tasks[-1].model_init()
+elif dataset == 'real':
+    for k in xrange(T):
+        with open(path_data + 'data' + str(k + 1), 'rb') as f:
+
+            reader = csv.reader(f)
+            for row in reader:
+                data_all[k].append(row[0].split(' '))
+        data_all[k].pop(0)  # remove first one
+
+        N = len(data_all[k])
+        for i in xrange(N):
+            del data_all[k][i][-1]
+            data_all[k][i] = [float(data_all[k][i][j])
+                              for j in xrange(d)]
+
+        with open(path_data + 'labelc' + str(k + 1), 'rb') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                label_all[k].append(float(row[0][:-1]))
+        label_all[k].pop(0)
+
+        tasks.append(task(path_results, dataset, data_all[k], label_all[k],
+                          conn, k, Lambda, ITER,
+                          p_ite, step_task, d, p_train,
+                          wait_time[k]))
+        tasks[-1].model_init()
+else:
+    raise Exception('Please choose the correct data sets!') # if choose incorrect data set
 
 server_p = Process(target=server_ins.run()) # create server process
 server_p.daemon = True # server_p will stop after main process stop
